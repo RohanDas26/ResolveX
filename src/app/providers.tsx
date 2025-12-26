a1aa783e53c0669848a993c410f3c02b76e83455
Rohan Das <rohandass2626@gmail.com>
2025-12-26 05:12:24 +0000

    fix error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,9 +4,6 @@ import { AuthProvider } from "@/hooks/use-auth";
 import { APIProvider } from "@vis.gl/react-google-maps";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  // IMPORTANT: You must create a .env.local file in the root of your project
-  // and add your Google Maps API key there.
-  // NEXT_PUBLIC_GOOGLE_MAPS_API_KEY=YOUR_API_KEY
   return (
     <AuthProvider>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
