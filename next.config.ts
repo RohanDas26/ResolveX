6a02e3f25ac5bd1afed93812693c031bba5d3283
Rohan Das <rohandass2626@gmail.com>
2025-12-27 08:55:26 +0000

    u know whhat check once for any errors internally wit api key etc if the

--- next.config.ts
+++ next.config.ts
@@ -10,31 +10,19 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'storage.googleapis.com',
+        hostname: 'api.dicebear.com',
         port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
+        hostname: 'placehold.co',
         port: '',
         pathname: '/**',
       }
